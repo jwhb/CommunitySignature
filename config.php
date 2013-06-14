7d5190756bb5e1e5110807bf23a23cda4ed8853d
Jasper Wiegratz <jwhy@jwhy.de>
2013-06-14 19:25:59 +0200

    Commented and extended config.php
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- config.php
+++ config.php
@@ -7,11 +7,29 @@ class Config{
 	}
 	
 	public static function getImageConfig(){
+
+		$white = array(255, 255, 255);
+		$black = array(0, 0, 0);
+		
 		return(array(
+			
+			/* Gravatar settings */
+			//'gravatar_url' => 'http://www.gravatar.com/avatar/{id}?s={size}',
+			'gravatar_url' => 'http://localhost/assets/jwhy.jpg',
+			'gravatar_size' => 100,
+				
+			/* Signature font settings */
 			'fontsize' => 10,
 			'fontfile' => './opensans.ttf',
-			'gravatar_url' => 'http://www.gravatar.com/avatar/{id}?s={size}',
-			'gravatar_size' => 100,
+				
+			/* Image dimensions */
+			'img_width' => 400,
+			'img_heigth' => 100,
+				
+			/* Colors */
+			'col' => array(
+				'background' => $white
+			),
 		));
 	}
 
